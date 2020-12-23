2fcbc7f38f7eb72593aeb81f410358b5f84f3ff9
Максим Сомов <catfishvip@mail.ru>
2020-12-23 22:25:08 +0200

    Добавьте файлы проекта.

---
+++ 7.2 basic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Задача организации, в особенности же новая модель, организационной деятельности. Способствует подготовке и реализации модели развития.";
+            int countSymbols = 0;
+            char[] symbols = { ',', '.' };
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < symbols.Length; j++)
+                {
+                    if (text[i].ToString() == symbols[j].ToString())
+                    {
+                        countSymbols++;
+                    }
+                }
+            }
+            Console.WriteLine(countSymbols);
+            Console.ReadKey();
+        }
+    }
+}
